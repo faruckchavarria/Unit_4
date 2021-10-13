c8b018cc29bb7c8c80e996819caf88d8f1d01043
faruckchavarria <evafaruck@gmail.com>
2021-10-13 14:40:13 -0600

    iniciar el projecto emprea para calcular el sueldo de un empleado

---
+++ src/main/java/org/faruck/Empleado.java
@@ -0,0 +1,15 @@
+package org.faruck;
+
+public class Empleado {
+
+    public String nombre;
+    public String apellido;
+    public String Cedula;
+    public String empleado;
+    public String horas_entras;
+    public String ventas_mes;
+
+
+
+
+}
